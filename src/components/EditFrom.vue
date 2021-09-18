2f078be17b8e4c6a7a12b5260ac61f0f9e73b42f
yuma <ikeoku.y@gmail.com>
2021-09-18 15:08:29 +0900

    ボタンのデザインを変更、テキストエリアの大きさを変更

--- src/components/EditFrom.vue
+++ src/components/EditFrom.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="edit-form">
-    <textarea cols="40" rows="10" v-model="editBody"></textarea>
+    <textarea cols="40" rows="20" v-model="editBody"></textarea>
     <button @click="edit()">編集</button>
     <button @click="destroy(editId)">削除</button>
   </div>
@@ -29,6 +29,15 @@ export default {
   margin: 0 auto;
 }
 textarea {
+  margin-top: 15px;
+  margin-bottom: 10px;
   display: block;
+}
+button {
+  margin-left: 5px;
+  padding: 0.3rem 2rem;
+  border-radius: 10px;
+  border-color: #ccc;
+  background: #eee;
 }
 </style>
